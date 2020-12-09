4bd6a139cb3952e48c9e4f19c944f2a2e69c2edb
Alexa Carrington <acarring@wellesley.edu>
2020-12-09 12:45:15 -0500

    Finished and edited checkIfPalindrome function in js

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const button = document.body.querySelector('section button');
 
 function checkIfPalindrome() {
     let inputText = input.value;
+
     if (inputText === "") {
         alert("You need to type in the input field to check for a palindrome.");
     };
 
+    if (inputText.replace(/[\W_]/g, "").toLowerCase() ===
+    inputText
+        .replace(/[\W_]/g, "")
+        .toLowerCase()
+        .split("")
+        .reverse()
+        .join("")) {
+            alert("You're right! That is, indeed, a palindrome.");
+        } else {
+            alert("Whoops... that ain't a palindrome.");
+        }
 }
 
 button.addEventListener('click', checkIfPalindrome);
